025396c446c4bd468f899f6b7f44d9edb8fe6b76
Aaron Hursh <amhursh@gmail.com>
2018-01-03 20:47:03 -0700

    Update exports

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
 import { View, Text } from 'react-native'
-import Header from './components/common/index'
+import { Header } from './components/common'
 
 class App extends Component {
   render() {
     return (
       <Header headerTitle={'CanvassCoord'} />
+      // <Text>Cool</Text>
     )
   }
 }
