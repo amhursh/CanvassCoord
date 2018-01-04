864a8d6eb936440e23dbeb9746e70546f169c966
Aaron Hursh <amhursh@gmail.com>
2018-01-03 20:58:08 -0700

    Add firebase initialize

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import React, { Component } from 'react'
 import { View, Text } from 'react-native'
+import firebase from 'firebase'
+
 import { Header } from './components/common'
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp(
+      {
+        apiKey: "AIzaSyARIGipyqxdnCwUYmZBwnaG8L_HlIFT49M",
+        authDomain: "canvasscoord.firebaseapp.com",
+        databaseURL: "https://canvasscoord.firebaseio.com",
+        projectId: "canvasscoord",
+        storageBucket: "canvasscoord.appspot.com",
+        messagingSenderId: "149344466621"
+      }
+    )
+  }
+
   render() {
     return (
       <Header headerTitle={'CanvassCoord'} />
