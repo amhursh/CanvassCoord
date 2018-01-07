4d95c18d22fefab0adf32174bf8706434a49a821
Aaron Hursh <amhursh@gmail.com>
2018-01-06 17:20:27 -0700

    Add boilerplate for target list

---
+++ src/components/TargetList.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
+
+class TargetList extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Targets go Here</Text>
+        <Text>Targets go Here</Text>
+        <Text>Targets go Here</Text>
+        <Text>Targets go Here</Text>
+        <Text>Targets go Here</Text>
+      </View>
+    )
+  }
+}
+
+export default TargetList
