2b84b19b4a2e96c865fcad838b4addb97a812ade
Aaron Hursh <amhursh@gmail.com>
2018-01-06 17:57:31 -0700

    Add navigation from login to targets list

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 import firebase from 'firebase'
+import { Actions } from 'react-native-router-flux'
 
 import { EMAIL_CHANGED, PASSWORD_CHANGED, LOGIN_USER_SUCCESS, LOGIN_USER_FAIL, LOGIN_USER } from './types'
 
@@ -31,6 +32,7 @@ const loginUserSuccess = (dispatch, user) => {
     type: LOGIN_USER_SUCCESS,
     payload: user
   })
+  Actions.targetList()
 }
 
 const loginUserFail = (dispatch, error) => {
