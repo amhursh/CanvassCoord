b25867c48e9618fd4953af2166b664a5e4b8fabf
Aaron Hursh <amhursh@gmail.com>
2018-01-08 02:50:39 -0700

    Add question list boilerplate

---
+++ src/components/QuestionList.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import { View, Text, FlatList } from 'react-native'
+import { connect } from 'react-redux'
+
+import { Spinner } from './common'
+// import QuestionItem from './QuestionItem'
+
+class QuestionList extends Component {
+  render() {
+    return (
+      <Text>Cool Beans</Text>
+    )
+  }
+}
+
+export default QuestionList
+
