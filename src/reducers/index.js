df9e4d3f776152f3dc924f6a798ce867363b99ea
Aaron Hursh <amhursh@gmail.com>
2018-01-08 00:45:31 -0700

    Add target reducer to reducers index

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -5,11 +5,13 @@ import CampaignReducer from './CampaignReducer'
 import SurveyReducer from './SurveyReducer'
 import CampaignSelectionReducer from './CampaignSelectionReducer'
 import SurveySelectionReducer from './SurveySelectionReducer'
+import TargetReducer from './TargetReducer'
 
 export default combineReducers({
   auth: AuthReducer,
   campaigns: CampaignReducer,
   surveys: SurveyReducer,
   selectedCampaignId: CampaignSelectionReducer,
-  selectedSurveyId: SurveySelectionReducer
+  selectedSurveyId: SurveySelectionReducer,
+  targets: TargetReducer
 })
