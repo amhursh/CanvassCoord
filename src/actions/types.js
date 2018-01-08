c15dd892ccd4d9212b10cdd8966193c7daf8dae7
Aaron Hursh <amhursh@gmail.com>
2018-01-08 01:14:41 -0700

    Add select target to types

--- src/actions/types.js
+++ src/actions/types.js
@@ -21,5 +21,7 @@ export const SELECT_CAMPAIGN = 'select_campaign'
 
 export const SELECT_SURVEY = 'select_survey'
 
+export const SELECT_TARGET = 'select_target'
+
 
 export const TARGET_FETCH_SUCCESS = 'target_fetch_success'
