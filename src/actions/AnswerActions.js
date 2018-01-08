dee8a54fb72ddd06a6a8ea99235b8765c8f7560c
Aaron Hursh <amhursh@gmail.com>
2018-01-08 05:02:10 -0700

    Update submit answer action with success logic

--- src/actions/AnswerActions.js
+++ src/actions/AnswerActions.js
@@ -1,6 +1,7 @@
 import { Actions } from 'react-native-router-flux'
+import axios from 'axios'
 
-import { ANSWER_CHANGED } from './types'
+import { ANSWER_CHANGED, SUBMIT_ANSWER, SUBMIT_ANSWER_SUCCESS } from './types'
 
 export const answerChanged = (text) => {
   return {
@@ -8,3 +9,17 @@ export const answerChanged = (text) => {
     payload: text
   }
 }
+
+export const submitAnswer = (answer, questionId, targetId) => {
+  return dispatch => {
+    axios.post(`http:10.0.2.2:8080/api/v1/answers`, {
+      answer: answer,
+      questionId: questionId,
+      targetId: targetId
+    })
+    .then(response => {
+      dispatch({ type: SUBMIT_ANSWER_SUCCESS, payload: response.data})
+    })
+  }
+}
+
